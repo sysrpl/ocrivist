a2d8c883551781e646e67f1400c3682810cdf257
malcolmgp@googlemail.com <malcolmgp@googlemail.com@da64c14e-01e9-8102-8d54-20bf99e6150d>
2011-05-13 08:50:16 +0000

    Scanner.pas: resolution and colour mode options now populated from scanner options

--- trunk/scanner.pas
+++ trunk/scanner.pas
@@ -13,16 +13,25 @@ type
   { TScannerForm }
 
   TScannerForm = class ( TForm )
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    ResolutionComboBox: TComboBox;
     OKButton: TBitBtn;
     ModeComboBox: TComboBox;
     DeviceComboBox: TComboBox;
-    ResolutionSpinEdit: TSpinEdit;
+    procedure DeviceComboBoxChange ( Sender: TObject ) ;
     procedure FormCreate ( Sender: TObject ) ;
   private
     { private declarations }
   public
     { public declarations }
-  end; 
+    OnChangeScanner: TNotifyEvent;
+    procedure InitResolution( saneoption: SANE_Option_Descriptor );
+    procedure InitMode( mode: SANE_Option_Descriptor );
+    function GetColorMode: string;
+    function GetResolution: Integer;
+  end;
 
 var
   ScannerForm: TScannerForm;
@@ -31,6 +40,8 @@ var
   devicecount: Integer;
 
 implementation
+
+uses ScanUtils;
 
 {$R *.lfm}
 
@@ -52,10 +63,117 @@ begin
        if devicecount>0 then
          begin
            DeviceComboBox.ItemIndex := 0;
-           sane_open(PChar(Devices[0]^.name), @ScannerHandle);
+           DeviceComboBoxChange(nil);
          end;
      end;
 
+end;
+
+procedure TScannerForm.InitResolution ( saneoption: SANE_Option_Descriptor ) ;
+type
+ wordlist = array [0..128] of SANE_Word;
+ pwordlist = ^wordlist;
+var
+  w: pwordlist;
+  x: Integer;
+  rangemin: Int64;
+  rangemax: Int64;
+  range: pSANE_Range;
+begin
+  ResolutionComboBox.Items.Clear;
+  if saneoption.name=SANE_NAME_SCAN_RESOLUTION then
+   case saneoption.constraint_type of
+        SANE_CONSTRAINT_RANGE:      begin
+                                       range := saneoption.prange;
+                                       case saneoption.option_type of
+                                            SANE_TYPE_INT :
+                                                  begin
+                                                     rangemin := range^.min;
+                                                     rangemax := range^.max;
+                                                  end;
+                                            SANE_TYPE_FIXED :
+                                                  begin
+                                                     rangemin := Trunc( SANE_UNFIX( range^.min));
+                                                     rangemax := Trunc( SANE_UNFIX( range^.max));
+                                                  end;
+                                            end;
+                                       if (rangemin<=150) and (rangemax>=150) then ResolutionComboBox.Items.Add('150');
+                                       if (rangemin<=300) and (rangemax>=300) then ResolutionComboBox.Items.Add('300');
+                                       if (rangemin<=600) and (rangemax>=600) then ResolutionComboBox.Items.Add('600');
+                                       if (rangemin<=1200) and (rangemax>=1200) then ResolutionComboBox.Items.Add('1200');
+                                    end;
+        SANE_CONSTRAINT_WORD_LIST:  begin
+                                       ResolutionComboBox.Items.Clear;
+                                       w := pwordlist(saneoption.pwordlist);
+//                                       ResolutionComboBox.Items.Add(IntToStr(w^[0]) + ' items in list:');
+                                       if saneoption.option_type=SANE_TYPE_INT then
+                                         for x := 1 to w^[0] do
+                                            ResolutionComboBox.Items.Add('    ' + IntToStr(w^[x]))
+                                       else
+                                       if saneoption.option_type=SANE_TYPE_FIXED then
+                                         for x := 1 to w^[0] do
+                                            ResolutionComboBox.Items.Add('    ' + FloatToStr( SANE_UNFIX(w^[x]) ) );
+                                    end;
+   end;
+   ResolutionComboBox.Enabled := ResolutionComboBox.Items.Count>0;
+   if ResolutionComboBox.Enabled then ResolutionComboBox.ItemIndex := ResolutionComboBox.Items.IndexOf('300') else ResolutionComboBox.ItemIndex := -1;
+end;
+
+procedure TScannerForm.InitMode ( mode: SANE_Option_Descriptor ) ;
+type
+ strlist = array[0..255] of PChar;
+ pstrlist = ^strlist;
+var
+  str: pstrlist;
+  RangeIndex: Integer;
+begin
+  ModeComboBox.Items.Clear;
+  if mode.name=SANE_NAME_SCAN_MODE then
+  if mode.constraint_type = SANE_CONSTRAINT_STRING_LIST then
+    begin
+       str := pstrlist(mode.pstringlist);
+       RangeIndex := 0;
+       while str^[RangeIndex] <> nil do
+             begin
+               ModeComboBox.Items.Add(str^[RangeIndex]);
+               Inc(RangeIndex);
+             end;
+       ModeComboBox.Enabled := ModeComboBox.Items.Count>0;
+    end;
+  if ModeComboBox.Enabled then ModeComboBox.ItemIndex := 0 else ModeComboBox.ItemIndex := -1;
+end;
+
+function TScannerForm.GetColorMode: string;
+begin
+  Result := '';
+  if ModeComboBox.ItemIndex>-1
+     then Result := ModeComboBox.Items[ModeComboBox.ItemIndex];
+end;
+
+function TScannerForm.GetResolution: Integer;
+begin
+  Result := 300;
+  if ResolutionComboBox.ItemIndex>-1
+     then Result := StrToInt(ResolutionComboBox.Items[ResolutionComboBox.ItemIndex]);
+end;
+
+procedure TScannerForm.DeviceComboBoxChange ( Sender: TObject ) ;
+var
+  scanmode: SANE_Option_Descriptor;
+  scanres: SANE_Option_Descriptor;
+  DevName: PChar;
+begin
+  if ScannerHandle<>nil then sane_close(ScannerHandle) ;
+  if DeviceComboBox.ItemIndex>-1 then
+     begin
+       DevName := Devices[DeviceComboBox.ItemIndex]^.name;
+       if sane_open(PChar(DevName), @ScannerHandle) = SANE_STATUS_GOOD then
+          begin
+            InitResolution(SaneGetOption(ScannerHandle, SANE_NAME_SCAN_RESOLUTION));
+            InitMode(SaneGetOption(ScannerHandle, SANE_NAME_SCAN_MODE));
+          end;
+     end;
+ if Assigned(OnChangeScanner) then OnChangeScanner(Self);
 end;
 
 end.
